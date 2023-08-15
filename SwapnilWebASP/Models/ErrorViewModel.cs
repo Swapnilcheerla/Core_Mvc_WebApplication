1dd2303e2a01d14bca8f70475c1ece46525d8db4
swapnil.cheerla <swapnil.cheerla@SWAPNILC>
2023-08-15 14:09:16 -0400

    Add project files.

---
+++ SwapnilWebASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SwapnilWebASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
