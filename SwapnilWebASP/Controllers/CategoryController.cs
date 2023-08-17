16f82c2a1993b9c32ab9d71558873893e19a532c
Swapnil Cheerla <cheerla.swapnilreddy@gmail.com>
2023-08-17 10:57:00 -0400

    added delete fuctionality and toastr

--- SwapnilWebASP/Controllers/CategoryController.cs
+++ SwapnilWebASP/Controllers/CategoryController.cs
@@ -32,6 +32,7 @@ namespace SwapnilWebASP.Controllers
 			{
 				_db.Categories.Add(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Category created successfully";
 				return RedirectToAction("Index");
 
 
@@ -46,7 +47,9 @@ namespace SwapnilWebASP.Controllers
 			{
 				return NotFound();
 			}
-			Category categorydb = _db.Categories.Find(id);
+			Category? categorydb = _db.Categories.Find(id);
+			//Category? categorydb1 = _db.Categories.FirstOrDefault(c => c.Id == id);
+			//Category? categorydb2 = _db.Categories.Where(c => c.Id == id).FirstOrDefault();
 			if (categorydb == null)
 			{
 				return NotFound();
@@ -56,20 +59,56 @@ namespace SwapnilWebASP.Controllers
 		[HttpPost]
 		public IActionResult Edit(Category obj)
 		{
-			if (obj.Name == obj.DisplayOrder.ToString())
-			{
-				ModelState.AddModelError("name", "The DisplayOrder exacly cannot match the name.");
-			}
+
+
 			if (ModelState.IsValid)
 			{
-				_db.Categories.Add(obj);
+				_db.Categories.Update(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Category updated successfully";
 				return RedirectToAction("Index");
 
 
 
 			}
 			return View();
+
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categorydb = _db.Categories.Find(id);
+			//Category? categorydb1 = _db.Categories.FirstOrDefault(c => c.Id == id);
+			//Category? categorydb2 = _db.Categories.Where(c => c.Id == id).FirstOrDefault();
+			if (categorydb == null)
+			{
+				return NotFound();
+			}
+			return View(categorydb);
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePost(int? id)
+		{
+			Category? obj = _db.Categories.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
+
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			TempData["success"] = "Category deleted successfully";
+			return RedirectToAction("Index");
+
+
+
+
+
 
 		}
 	}
