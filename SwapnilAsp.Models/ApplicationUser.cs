759924387f481445ee37d572c9ad459031070d87
SwapnilCheerla <cheerla.swapnilreddy@nypti.org>
2024-02-29 14:52:16 -0500

    user controller

--- SwapnilAsp.Models/ApplicationUser.cs
+++ SwapnilAsp.Models/ApplicationUser.cs
@@ -5,19 +5,21 @@ using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace SwapnilAsp.Models
 {
-	public class ApplicationUser : IdentityUser
-	{
-		[Required]
-		public string? Name { get; set; }
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string? Name { get; set; }
 
-		public string? StreetAddress { get; set; }
-		public string? City { get; set; }
-		public string? State { get; set; }
-		public string? PostalCode { get; set; }
+        public string? StreetAddress { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
 
-		public int? CompanyId { get; set; }
-		[ForeignKey("CompanyId")]
-		[ValidateNever]
-		public Company Company { get; set; }
-	}
+        public int? CompanyId { get; set; }
+        [ForeignKey("CompanyId")]
+        [ValidateNever]
+        public Company? Company { get; set; }
+        [NotMapped]
+        public string Role { get; set; }
+    }
 }
---
+++ SwapnilWebASP/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SwapnilAsp.DataAccess.Repository.IRepository;
+using SwapnilAsp.Models;
+using SwapnilAsp.Utility;
+
+namespace SwapnilWebASP.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IUnitOfWork _unitOfWork;
+        public UserController(UserManager<IdentityUser> userManager, IUnitOfWork unitOfWork, RoleManager<IdentityRole> roleManager)
+        {
+            _unitOfWork = unitOfWork;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _unitOfWork.ApplicationUser.GetAll(includeProperties: "Company").ToList();
+
+            foreach (var user in objUserList)
+            {
+
+                user.Role = _userManager.GetRolesAsync(user).GetAwaiter().GetResult().FirstOrDefault();
+
+                if (user.Company == null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = ""
+                    };
+                }
+            }
+
+            return Json(new { data = objUserList });
+        }
+
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string id)
+        {
+
+            var objFromDb = _unitOfWork.ApplicationUser.Get(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }
+
+            if (objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                //user is currently locked and we need to unlock them
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            //_unitOfWork.ApplicationUser.Update(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Operation Successful" });
+        }
+
+        #endregion
+    }
+}
